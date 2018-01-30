3e163be4bfd06389ecf4ec82ff2b8f8a414f2f2b
阿波連智恵 <aharenchie@harenchi.local>
2018-01-31 01:25:14 +0900

    view2を追加する

--- eval_project/sound_eval/urls.py
+++ eval_project/sound_eval/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . import views 
+from . import views,views2 
 
 app_name = 'sound_eval'
 urlpatterns = [
     path('', views.index),
     path('practice_announcement/', views.practice_announcement,name='practice_announcement'),
     path('practice/',views.practice,name='practice'),
+    path('practice2/',views2.practice,name='practice2'),
     path('end/',views.end,name='end'),
 ]    
